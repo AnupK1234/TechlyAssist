30a1429b212f0f8150067881907ce71b320f3ee4
Anup Khismatrao <khismatrao2003@gmail.com>
2024-06-06 15:05:01 +0530

    Feat: Card Stack component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-black font-bold text-center">Card Stack Component from Aceternity UI</h1>
+      <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CardStack.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
+export const CardStack = ({
+  items,
+  offset,
+  scaleFactor,
+}) => {
+  const CARD_OFFSET = offset || 10;
+  const SCALE_FACTOR = scaleFactor || 0.06;
+  const [cards, setCards] = useState(items);
+  let interval= null;
+
+  useEffect(() => {
+    startFlipping();
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const startFlipping = () => {
+    interval = setInterval(() => {
+      setCards((prevCards) => {
+        const poppedElement = prevCards.pop?.();
+        if (poppedElement !== undefined) {
+          const newArray = [...prevCards];
+          newArray.unshift(poppedElement);
+          return newArray;
+        } else {
+          // Handle the case where the array is empty (optional)
+          return prevCards;
+        }
+      });
+    }, 3000);
+  };
+
+  return (
+    <div className="relative h-60 w-60 md:h-60 md:w-96">
+      {cards.map((card, index) => (
+        <motion.div
+          key={card.id}
+          className="absolute dark:bg-black bg-white h-60 w-60 md:h-60 md:w-96 rounded-3xl p-4 shadow-xl border border-neutral-200 dark:border-white/[0.1]  shadow-black/[0.1] dark:shadow-white/[0.05] flex flex-col justify-between"
+          style={{
+            transformOrigin: "top center",
+          }}
+          animate={{
+            top: index * -CARD_OFFSET,
+            scale: 1 - index * SCALE_FACTOR, // decrease scale for cards that are behind
+            zIndex: cards.length - index, // decrease z-index for the cards that are behind
+          }}
+        >
+          <div className="font-normal text-neutral-700 dark:text-neutral-200">
+            {card.content}
+          </div>
+          <div>
+            <p className="text-neutral-500 font-medium dark:text-white">
+              {card.name}
+            </p>
+            <p className="text-neutral-400 font-normal dark:text-neutral-200">
+              {card.designation}
+            </p>
+          </div>
+        </motion.div>
+      ))}
+    </div>
+  );
+};
